99f8552fe620592aa61acb3e0fea844efa1b8d5a
vitaliisavchuk <vitaliisavchuk@cloud.upwork.com>
2017-04-04 18:45:02 +0300

    Issue #8: add html templates

--- server/services/mail/index.js
+++ server/services/mail/index.js
@@ -3,8 +3,27 @@ import path from 'path';
 import async from 'async';
 import nodemailer from 'nodemailer';
 import winston from 'winston';
+import { minify } from 'html-minifier';
+import cssmin from 'cssmin';
+import juice from 'juice';
+import handlebars from 'handlebars';
 
 import config from '../../../config/env';
+
+const htmlMinOpts = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeEmptyAttributes: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true
+};
+
+const juiceOpts = {
+};
 
 export default
 class MailService {
@@ -17,6 +36,32 @@ class MailService {
     winston.info(`Mail service initialized "${this.options.transport.service}"`);
   }
 
+  loadPartials(next) {
+    const partialPath = path.join(this.options.pluginsPath, 'partials');
+
+    fs.readdir(partialPath, (err, files) => {
+      if (err) {
+        return next(err);
+      }
+
+      return async.each(files, (fileName, nextFile) => {
+        winston.info(`[MailService] loading partial "${fileName}"...`);
+
+        fs.readFile(path.join(partialPath, fileName), { encoding: 'utf8' }, (fileErr, data) => {
+          if (fileErr) {
+            return nextFile(fileErr);
+          }
+
+          const content = (path.extname(fileName) === '.css') ? cssmin(data) : data;
+
+          handlebars.registerPartial(fileName, content);
+          winston.info(`[MailService] partial "${fileName}" loaded.`);
+          return nextFile();
+        });
+      }, next);
+    });
+  }
+
   sendTemplate(templateName, sendToEmail, cb) {
     const sendOptions = {
       from: this.options.noReply,
@@ -26,7 +71,22 @@ class MailService {
     };
 
     async.auto({
-      txtTemplate: next => this.getTemplate(templateName, 'txt', next)
+      htmlTemplate: next => this.getTemplate(templateName, 'html', next),
+      txtTemplate: next => this.getTemplate(templateName, 'txt', next),
+      htmlEmail: ['htmlTemplate', (data, next) => {
+        if (!data.htmlTemplate) {
+          return next();
+        }
+
+        let mailHtml = data.htmlTemplate({});
+        return juice.juiceResources(mailHtml, juiceOpts, (err, html) => {
+          if (err) { return next(err); }
+
+          mailHtml = minify(html, htmlMinOpts);
+          sendOptions.html = mailHtml;
+          return next();
+        });
+      }]
     }, (err, data) => {
       if (err) {
         winston.error(err);
@@ -34,8 +94,8 @@ class MailService {
       }
 
       if (data.txtTemplate) {
-        sendOptions.text = data.txtTemplate;
-      } else {
+        sendOptions.text = data.txtTemplate({});
+      } else if (!data.htmlTemplate) {
         return cb({ message: `Template ${templateName} not found` });
       }
 
@@ -63,7 +123,7 @@ class MailService {
         if (err) {
           return next(err);
         }
-        return next(null, data);
+        return next(null, handlebars.compile(data));
       });
     });
   }
--- server/services/mail/mail.test.js
+++ server/services/mail/mail.test.js
@@ -14,7 +14,7 @@ describe('## MailService', () => {
 
     const testMail = 'esvit666@gmail.com';
 
-    sinon.stub(service.transporter, 'sendMail').callsFake(() => {
+    const stub = sinon.stub(service.transporter, 'sendMail').callsFake(() => {
       expect(service.transporter.sendMail.calledOnce).to.be.true; // eslint-disable-line no-unused-expressions
 
       expect(service.transporter.sendMail.getCall(0).args[0]).to.deep.equal({
@@ -24,9 +24,39 @@ describe('## MailService', () => {
         xMailer: 'Mailer',
         text: 'Hi, this is test message!\n'
       });
+      stub.restore();
       done();
     });
 
-    service.sendTemplate('mail.test.message', testMail, () => {});
+    service.sendTemplate('mail.test.txtmessage', testMail, () => {
+    });
+  });
+
+  it('should send html email', (done) => {
+    const service = new MailService();
+
+    const testMail = 'esvit666@gmail.com';
+
+    service.loadPartials(() => {
+      sinon.stub(service.transporter, 'sendMail').callsFake(() => {
+        expect(service.transporter.sendMail.calledOnce).to.be.true; // eslint-disable-line no-unused-expressions
+
+        const arg0 = service.transporter.sendMail.getCall(0).args[0];
+        const html = arg0.html;
+        delete arg0.html;
+
+        expect(arg0).to.deep.equal({
+          from: 'Chat <chat@eliftech.com>',
+          to: testMail,
+          subject: 'Notification',
+          xMailer: 'Mailer'
+        });
+        expect(html).to.match(/Hi there/);
+        done();
+      });
+
+      service.sendTemplate('mail.test.htmlmessage', testMail, () => {
+      });
+    });
   });
 });
